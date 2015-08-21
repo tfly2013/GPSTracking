b37c6c7aef335ec8d6e46a0f0bebc7140caff3bd
Fei Tang <tfly2012@hotmail.com>
2015-08-21 21:35:21 +1000

    Web - Add bootstrap

--- GPS-Tracking-Orange-Web/config/routes.rb
+++ GPS-Tracking-Orange-Web/config/routes.rb
@@ -4,9 +4,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   namespace :api do
-    scope :v1 do
       mount_devise_token_auth_for 'User', at: 'auth'
-    end
   end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
