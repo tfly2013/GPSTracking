0fa2d6da865953733d6cab0e64854c73ffcdf465
Fei Tang <tfly2012@hotmail.com>
2015-09-14 13:39:44 +0800

    Map - add some documentations to js behind map.

--- GPS-Tracking-Orange-Web/app/assets/javascripts/trips.js
+++ GPS-Tracking-Orange-Web/app/assets/javascripts/trips.js
@@ -9,6 +9,9 @@ var zones = [];
 var segments = [];
 var map;
 
+/***
+* Initialize map
+*/
 function initMap(){
 	map = new google.maps.Map(document.getElementById('map'), {
 		zoom: 12,
@@ -23,7 +26,13 @@ function initMap(){
 	});
 }
 
+/*** 
+* Phase coordinates into 2D array
+* coordinates: 2D array of google map LatLng object
+* coordinates[i]: coordinates for a segement
+*/
 function phaseCoordinates(){	
+	
 	var temp = [];
 	if (coordinates.length == 0){
 		return;
@@ -34,6 +43,8 @@ function phaseCoordinates(){
 		var latlng = new google.maps.LatLng(
 			coordinates[i].lat,coordinates[i].lng);
 		latlng.id = i;
+		// coordinates has been sorted, so same coordinate with segment would stay together.
+		// new segment is created if incoming location has a different segement id.
 		if (segment.id != coordinates[i].seg){		
 			segment.push(latlng);		
 			temp.push(segment);
@@ -46,6 +57,9 @@ function phaseCoordinates(){
 	coordinates = temp;	
 }
 
+/*** 
+* Zoom the map to show the trip
+*/
 function zoomToCoordinates(){	
 	var bounds = new google.maps.LatLngBounds();
 	for (var i = 0; i < coordinates.length ; i++){
@@ -55,6 +69,9 @@ function zoomToCoordinates(){
 	map.fitBounds(bounds);
 }
 
+/*** 
+* Draw each segement (polyline)
+*/
 function drawSegments() {
 	for (var i = 0; i < coordinates.length; i++){
 		segment = new google.maps.Polyline({
@@ -65,6 +82,7 @@ function drawSegments() {
 		segment.setMap(map);
 		segment.segId = coordinates[i].id;
 		segment.id = i;
+		// segment click event, pop up a infowindow which allows to change transportation
 		google.maps.event.addListener(segment, 'click', function(event) { 
 			var form = $(".seg-form-" + this.segId).clone().show()[0];
 			var infowindow = new google.maps.InfoWindow();
@@ -76,6 +94,9 @@ function drawSegments() {
 	}
 }
 
+/*** 
+* Draw each transfer zones (marker)
+*/
 function drawZones(){
 	// The start position
 	var zone = new google.maps.Marker({
@@ -112,12 +133,14 @@ function drawZones(){
 	zones.push(zone);	
 }
 
-
+/** snap zones to segments, make zones can only move along segments
+*/
 function snapZonesToSegments(){
 	for (var i = 1; i < zones.length - 1; i++){
 		zone = zones[i];
 		var before = zone.segBefore.getPath().getArray();
 		var after = zone.segAfter.getPath().getArray();
+		// the moving range of a zone is segment before plus segment after
 		merged = new google.maps.Polyline({
 			path: before.concat(after)
 		});
@@ -131,12 +154,16 @@ function snapZonesToSegments(){
 	}
 }
 
-
+/** Update segment after zone has changed position
+*/
 function updateSegments(zone){
+	// zone's current position is between point[n] and point[n-1] along the line that it snapped to
 	var n = zone.str.getPointPosition();
 	var segment;
+	// move to segment before or after
 	var before = false;
 	if (n > zone.segBefore.getPath().length){
+		// move after, determine the real n on segAfter
 		n-= zone.segBefore.getPath().length;
 		segment = zone.segAfter;
 	}
@@ -148,17 +175,25 @@ function updateSegments(zone){
 	var latlng = zone.getPosition();
 	var removed;
 	if (before){
+		// remove the last point of segBefore (which is same as the first point of segAfter)
 		coordinates[id].pop();
+		// rmeove points from the end of segBefore, add current zone point at last
 		removed = coordinates[id].splice(n, coordinates[id].length-n, latlng);
+		// remove the first point of segAfter, if it's a new point(no id), 
+		// which mean it was inserted by last zone move
 		if (coordinates[id+1][0].id == null){
 			coordinates[id+1].shift();
 		}
+		// add removed points to segAfter
 		coordinates[id+1] = removed.concat(coordinates[id+1]);
+		// insert current zone point at the beginning of segAfter
 		coordinates[id+1].unshift(latlng);
+		// update both segments
 		segments[id].setPath(coordinates[id]);
 		segments[id+1].setPath(coordinates[id+1]);
 	}
 	else{
+		// similar to before
 		coordinates[id].shift();
 		removed = coordinates[id].splice(0, n, latlng);
 		if (coordinates[id-1][coordinates[id-1].length - 1].id == null){
@@ -170,10 +205,13 @@ function updateSegments(zone){
 		segments[id-1].setPath(coordinates[id-1]);
 	}
 
+	// update the moving range of zone before and zone after
 	updateStr(zones[zone.id - 1]);
 	updateStr(zones[zone.id + 1]);
 }
 
+/** Update snap to route object to update the moving range of a zone
+*/
 function updateStr(zone){
 	if (zone.fixed)
 		return;	
@@ -183,8 +221,14 @@ function updateStr(zone){
 }
 
 /** 
-*	Modified Snap to route library
-*/
+ * Modified Snap to route library
+ *
+ * @constructor
+ * @desc Creates a new SnapToRoute that will snap the marker to the route.
+ * @param {GMap2} map Map to assign listeners to.
+ * @param {GMarker} marker Marker to move along the route.
+ * @param {GPolyline} polyline The line the marker should snap to.
+ */
 function SnapToRoute(map, marker, polyline) {
 	this.routePixels_ = [];
 	this.normalProj_ = map.getProjection();
@@ -195,22 +239,40 @@ function SnapToRoute(map, marker, polyline) {
 	this.init_();
 }
 
+/**
+ * Initialize the objects.
+ * @private
+ */ 
 SnapToRoute.prototype.init_ = function () {
 	this.loadLineData_();
 	this.loadMapListener_();
 };
 
+/**
+ * Change the marker and/or polyline used by the class.
+ * @param {GMarker} marker Optional marker to move along the route, 
+ *   or null if you do not want to change that target.
+ * @param {GPolyline} polyline Optional line to snap to, 
+ *   or null if you do not want to change that target.
+ */
 SnapToRoute.prototype.updateTargets = function (marker, polyline) {
 	this.marker_ = marker || this.marker_;
 	this.polyline_ = polyline || this.polyline_;
 	this.loadLineData_();
 };
 
+/**
+ * update the polyline data.
+ */
 SnapToRoute.prototype.updateLine = function (path) {
 	this.polyline_.setPath(path);
 	this.loadLineData_();
 };
 
+/**
+ * Set up map listeners to calculate and update the marker position.
+ * @private
+ */
 SnapToRoute.prototype.loadMapListener_ = function () {
 	var me = this;
 
@@ -227,6 +289,11 @@ SnapToRoute.prototype.loadMapListener_ = function () {
 	});
 };
 
+/**
+ * Load route pixels into array for calculations. 
+ * This needs to be calculated whenever zoom changes 
+ * @private
+ */
 SnapToRoute.prototype.loadLineData_ = function () {
 	var zoom = this.map_.getZoom();
 	this.routePixels_ = [];
@@ -237,22 +304,39 @@ SnapToRoute.prototype.loadLineData_ = function () {
 	}
 };
 
+/**
+ * Handle the move listener output and move the given marker.
+ * @param {GLatLng} mouseLatLng
+ * @private
+ */
 SnapToRoute.prototype.updateMarkerLocation_ = function (mouseLatLng) {
 	var markerLatLng = this.getClosestLatLng(mouseLatLng);
 	this.marker_.setPosition(markerLatLng);
 };
 
+/**
+ * Calculate closest lat/lng on the polyline to a test lat/lng.
+ * @param {GLatLng} latlng The coordinate to test.
+ * @return {GLatLng} The closest coordinate.
+ */
 SnapToRoute.prototype.getClosestLatLng = function (latlng) {
 	var r = this.distanceToLines_(latlng);
 	return this.normalProj_.fromPointToLatLng(new google.maps.Point(r.x, r.y));
 };
 
+/** @return the nearest segment which indicates the position of marker
+*/
 SnapToRoute.prototype.getPointPosition = function () {
 	latlng = this.marker_.getPosition();
 	var r = this.distanceToLines_(latlng);
 	return r.i;
 }
 
+/**
+ * Gets test pixel and then calls fundamental algorithm.
+ * @param {GLatLng} mouseLatLng
+ * @private
+ */
 SnapToRoute.prototype.distanceToLines_ = function (mouseLatLng) {
 	var zoom = this.map_.getZoom();
 	var mousePx = this.normalProj_.fromLatLngToPoint(mouseLatLng);
@@ -260,6 +344,17 @@ SnapToRoute.prototype.distanceToLines_ = function (mouseLatLng) {
 	return this.getClosestPointOnLines_(mousePx, routePixels_);
 };
 
+/**
+ * Static function. Find point on lines nearest test point
+ * test point pXy with properties .x and .y
+ * lines defined by array aXys with nodes having properties .x and .y 
+ * return is object with .x and .y properties and property i indicating nearest segment in aXys 
+ * and property from the fractional distance of the returned point from aXy[i-1]
+ * and property to the fractional distance of the returned point from aXy[i]    
+ * @param {Object} pXy
+ * @param {Array<Point>} aXys
+ * @private
+ */
 SnapToRoute.prototype.getClosestPointOnLines_ = function (pXy, aXys) {
 	var minDist;
 	var to;
