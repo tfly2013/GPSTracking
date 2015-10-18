9dbe11e41aa88210199e14a02faba614e0e8b782
jackwarpten@gmail.com <5321warpten>
2015-10-19 02:53:53 +1100

    try commit

--- GPS-Tracking-Orange-Web/app/controllers/reports_controller.rb
+++ GPS-Tracking-Orange-Web/app/controllers/reports_controller.rb
@@ -9,87 +9,38 @@ class ReportsController < ApplicationController
        segments = Segment.all
        @average_distance = average_distance(segments)
        @average_speed = average_speed(segments)
-       
-       #@modes = modes_count(segments)
+       @modes = modes_count(segments)
+       datasetItems = @modes.each do |mode, count|
+         { 
+           chart: {
+                caption: "Transportation Mode Statistics",
+                xAxisname: "Mode",
+                yAxisName: "Count",
+                theme: "fint",
+                exportEnabled: "1",
+              },
+           categories: [{
+             category: [{ label: mode}]
+              }],
+           dataset: [{
+             seriesname: "Modes",
+                data: [{ value: count }]
+           }]
+         }
+         end
        @chart = Fusioncharts::Chart.new({
-        :height => 400,
-        :width => 600,
-        :id => 'chart',
-        :type => 'column2d',
-        :renderAt => 'chart-container', 
-        :dataSource => '{
-          "chart": {
-            "caption": "Transportation Mode Statistics",
-            "xAxisName": "Mode",
-            "yAxisName": "Count",
-            "numberPrefix": "",
-            "paletteColors": "#0075c2",
-            "bgColor": "#ffffff",
-            "borderAlpha": "20",
-            "canvasBorderAlpha": "0",
-            "usePlotGradientColor": "0",
-            "plotBorderAlpha": "10",
-            "placevaluesInside": "1",
-            "rotatevalues": "1",
-            "valueFontColor": "#ffffff",
-            "showXAxisLine": "1",
-            "xAxisLineColor": "#999999",
-            "divlineColor": "#999999",
-            "divLineDashed": "1",
-            "showAlternateHGridColor": "0",
-            "subcaptionFontBold": "0",
-            "subcaptionFontSize": "14"
-          },
-          "data": [{
-            "label": "Jan",
-            "value": "420000"
-          }, {
-            "label": "Feb",
-            "value": "810000"
-          }, {
-            "label": "Mar",
-            "value": "720000"
-          }, {
-            "label": "Apr",
-            "value": "550000"
-          }, {
-            "label": "May",
-            "value": "910000"
-          }, {
-            "label": "Jun",
-            "value": "510000"
-          }, {
-            "label": "Jul",
-            "value": "680000"
-          }, {
-            "label": "Aug",
-            "value": "620000"
-          }, {
-            "label": "Sep",
-            "value": "610000"
-          }, {
-            "label": "Oct",
-            "value": "490000"
-          }, {
-            "label": "Nov",
-            "value": "900000"
-          }, {
-            "label": "Dec",
-            "value": "730000"
-          }]
-        }'
+          width: "600",
+          height: "400",
+          type: "mscolumn2d",
+          renderAt: "chart-container"     
        })
+       @chart.dataSource = datasetItems.to_json
+      
        render :action => "researcher_index"
     else
-       #trip = Trip.where("user_id = ?", current_user.id)
        @trips = current_user.trips
-       
        render :action => "user_index"
     end
-    
-
-
-
   end
 
   def admin_and_researcher_only
