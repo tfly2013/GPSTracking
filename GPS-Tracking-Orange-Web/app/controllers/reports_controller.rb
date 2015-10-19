b9348409cd3dd21b9d310f5d9d6a103efa4da39f
jackwarpten@gmail.com <5321warpten>
2015-10-19 14:46:45 +1100

    user report done, researcher report chart doesn't display atm

--- GPS-Tracking-Orange-Web/app/controllers/reports_controller.rb
+++ GPS-Tracking-Orange-Web/app/controllers/reports_controller.rb
@@ -10,31 +10,49 @@ class ReportsController < ApplicationController
        @average_distance = average_distance(segments)
        @average_speed = average_speed(segments)
        @modes = modes_count(segments)
-       datasetItems = @modes.each do |mode, count|
-         { 
-           chart: {
-                caption: "Transportation Mode Statistics",
-                xAxisname: "Mode",
-                yAxisName: "Count",
-                theme: "fint",
-                exportEnabled: "1",
-              },
-           categories: [{
-             category: [{ label: mode}]
-              }],
-           dataset: [{
-             seriesname: "Modes",
-                data: [{ value: count }]
-           }]
-         }
-         end
        @chart = Fusioncharts::Chart.new({
           width: "600",
           height: "400",
           type: "mscolumn2d",
-          renderAt: "chart-container"     
+          renderAt: "chart-container",
+          chart: {
+            caption: "Comparison of Quarterly Revenue",
+            subCaption: "Harry's SuperMart",
+            xAxisname: "Quarter",
+            yAxisName: "Amount ($)",
+            numberPrefix: "$",
+            theme: "fint",
+            exportEnabled: "1",
+            },
+            categories: [{
+                  category: [
+                      { label: "Q1" },
+                      { label: "Q2" },
+                      { label: "Q3" },
+                      { label: "Q4" }
+                  ]
+                }],
+            dataset: [
+                {
+                    seriesname: "Previous Year",
+                    data: [
+                        { value: "10000" },
+                        { value: "11500" },
+                        { value: "12500" },
+                        { value: "15000" }
+                    ]
+                },
+                {
+                    seriesname: "Current Year",
+                    data: [
+                        { value: "25400" },
+                        { value: "29800" },
+                        { value: "21800" },
+                        { value: "26800" }
+                    ]
+                }
+          ] 
        })
-       @chart.dataSource = datasetItems.to_json
       
        render :action => "researcher_index"
     else
